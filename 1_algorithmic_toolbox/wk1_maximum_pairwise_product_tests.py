74db2a29a0e0a6cf87d814cc77c9992e70567c3a
Luke Petschauer <luke.petschauer@me.com>
2016-07-07 14:37:42 -0700

    a few more tests; get rid of unnecessary stuff.

--- 1_algorithmic_toolbox/wk1_maximum_pairwise_product_tests.py
+++ 1_algorithmic_toolbox/wk1_maximum_pairwise_product_tests.py
@@ -1,20 +1,27 @@
+"""Tests for wk1 mpp function."""
+
+
 import unittest
 from .wk1_maximum_pairwise_product import mpp
 
 
 class testMPP(unittest.TestCase):
+    """Our testcases."""
 
     def setUp(self):
+        """To be used across test cases."""
         self.even_list = [5, 1, 3, 2, 4]
         self.weird_list = [5, 1, 3, 2, 4, 9]
         self.odd_list = [5, 4, 3]
         self.empty_list = []
+        self.evil_list = [9, 0, 0, 10]
 
     def test_if_an_even_list_works(self):
+        """Check list with even number of items."""
         self.assertEqual(mpp(self.even_list), 20)
 
-    def test_if_an_odd_list_works(self):
-        """Does it work if there are an odd number of items in the list?"""
+    def test_an_odd_list_works(self):
+        """Check list with odd number of items."""
         self.assertEqual(mpp(self.odd_list), 20)
 
     def test_if_an_empty_list_works(self):
@@ -22,7 +29,12 @@ class testMPP(unittest.TestCase):
         self.assertEqual(mpp(self.empty_list), 0)
 
     def test_weird_list(self):
+        """Check what happens when last number is largest."""
         self.assertEqual(mpp(self.weird_list), 45)
+
+    def test_evil_list(self):
+        """Test an evil list."""
+        self.assertEqual(mpp(self.evil_list), 90)
 
 
 if __name__ == '__main__':
