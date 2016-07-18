1daf2d3597b34b92f59bcab92726bb3e767bbbce
Luke Petschauer <luke.petschauer@me.com>
2016-07-17 17:33:49 -0700

    made the algorithm greedy. runs in half the time of the previous algo.

--- 1_algorithmic_toolbox/wk3_change.py
+++ 1_algorithmic_toolbox/wk3_change.py
@@ -1,12 +1,27 @@
 # Uses python3
 import sys
 
-def get_change(m):
-    coins = 0
-    if m % 10 != 0:
-        pass
+def get_change(m, coins):
+
+    while m:
+        if m - 10 >= 0:
+            m -= 10
+            coins += 1
+            return get_change(m, coins)
+        if m - 5 >= 0:
+            m -= 5
+            coins += 1
+            return get_change(m, coins)
+        if m - 1 >= 0:
+            m -= 1
+            coins += 1
+            return get_change(m, coins)
+
     return coins
 
+    # The below is a one-line answer...but it isn't greedy!
+    # return num % 5+((num-(num % 5)) % 10 // 5) + ((num - (num % 10)) // 10)
+
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    num = int(sys.stdin.read())
+    print(get_change(num, 0))
